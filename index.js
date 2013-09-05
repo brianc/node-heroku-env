192430f17e5ca0647a23bd67f848e640cc6b67ef
Brian Carlson <brian.m.carlson@gmail.com>
2013-09-05 15:43:01 -0500

    Make it werk

--- index.js
+++ index.js
@@ -1,26 +1,17 @@
-var exec = require('child_process').exec
 var parse = require('parse-database-url')
+var get = require('./lib/get')
 
-exec('heroku config --app ' + process.argv[2], function(err, stdout) {
-  if(err) throw err;
-  var config = {}
-  var lines = stdout.split('\n')
-  lines.shift()
-  lines.forEach(function(line) {
-    if(!line.trim()) return;
-    var parts = line.split(': ')
-    if(!parts[1]) {
-      console.log('could not parse', line)
-    } else {
-      config[parts[0].trim()] = parts[1].trim()
-    }
+module.exports = function(app, cb) {
+  return get(app, function(err, config) {
+    if(err) return cb(err);
+    var params = parse(config.DATABASE_URL)
+    config.PGDATABASE = params.database
+    config.PGPASSWORD = params.password
+    config.PGHOST = params.host
+    config.PGPORT = params.port
+    config.PGUSER = params.user
+    config.PGSSLMODE = 'require'
+    cb(null, config)
   })
-  var params = parse(config.DATABASE_URL)
-  var str = ""
-  str += 'PGDATABASE=' + params.database
-  str += ' PGHOST=' + params.host
-  str += ' PGPASSWORD=' + params.password
-  str += ' PGPORT=' + params.port
-  str += ' PGUSER=' + params.user
-  console.log(str)
-})
+}
+
