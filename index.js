f2315054b1d880fa91c3ab9648b234c40cc3a4e2
Brian Carlson <brian.m.carlson@gmail.com>
2013-09-05 22:55:31 -0500

    Be forgiving if there is no database

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 var parse = require('parse-database-url')
 var get = require('./lib/get')
 
+var getPostgresConfig = function(config) {
+  for(var key in config) {
+    if(key.indexOf('HEROKU_POSTGRES') === 0 || key.indexOf('DATABASE_URL') === 0) {
+      var params = parse(config[key])
+      config.PGDATABASE = params.database
+      config.PGPASSWORD = params.password
+      config.PGHOST = params.host
+      config.PGPORT = params.port
+      config.PGUSER = params.user
+      config.PGSSLMODE = 'require'
+      return
+    }
+  }
+}
+
 module.exports = function(app, cb) {
   return get(app, function(err, config) {
     if(err) return cb(err);
-    var params = parse(config.DATABASE_URL)
-    config.PGDATABASE = params.database
-    config.PGPASSWORD = params.password
-    config.PGHOST = params.host
-    config.PGPORT = params.port
-    config.PGUSER = params.user
-    config.PGSSLMODE = 'require'
+    getPostgresConfig(config)
     cb(null, config)
   })
 }
